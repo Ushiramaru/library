9d02bc1e3f9d106ae7cc0aaf4725c83cef874fe7
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-11-09 00:56:23 +0300

    Deleted duplicated code

---
+++ src/main/java/controller/command/impl/AddNewEdition.java
@@ -0,0 +1,34 @@
+package controller.command.impl;
+
+import bean.*;
+import bean.enums.*;
+import controller.command.Command;
+import service.LibraryService;
+import service.factory.ServiceFactory;
+import service.exeption.ServiceException;
+
+public class AddNewEdition implements Command {
+
+    @Override
+    public String execute(String request) {
+        String response;
+        try {
+            Edition edition = EditionCreator.valueOf(request);
+
+            ServiceFactory factory = ServiceFactory.getInstance();
+            LibraryService service = factory.getLibraryService();
+
+            service.addNewEdition(edition);
+            response = "Added completed";
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            response = "Error during added procedure";
+        } catch (IllegalArgumentException e) {
+            response = "Illegal arguments";
+        } catch (ServiceException e) {
+            response = "Error during added procedure. " + e.getMessage().substring(e.getMessage().indexOf(paramDelimiter) + 1);
+        }
+
+        return response;
+    }
+
+}
---
+++ src/main/java/controller/command/impl/EditionCreator.java
@@ -0,0 +1,58 @@
+package controller.command.impl;
+
+import bean.*;
+import bean.enums.*;
+import controller.command.Command;
+
+class EditionCreator {
+
+    static Edition valueOf(String s) {
+        int indexStart = s.indexOf(Command.paramDelimiter) + 1;
+        int indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        EditionType type = EditionType.valueOf(s.substring(indexStart, indexEnd).toUpperCase());
+
+        indexStart = indexEnd + 1;
+        indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        long id = Long.valueOf(s.substring(indexStart, indexEnd));
+
+        indexStart = indexEnd + 1;
+        indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        String title = s.substring(indexStart, indexEnd);
+
+        indexStart = indexEnd + 1;
+        indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        ListFormat listFormat = ListFormat.valueOf(s.substring(indexStart, indexEnd).toUpperCase());
+
+        indexStart = indexEnd + 1;
+        indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        int listCount = Integer.valueOf(s.substring(indexStart, indexEnd));
+
+        indexStart = indexEnd + 1;
+        indexEnd = s.indexOf(Command.paramDelimiter, indexStart);
+        int publicationYear = Integer.valueOf(s.substring(indexStart, indexEnd));
+
+        Edition edition;
+        switch (type) {
+            case PRINT_EDITION:
+                edition = new PrintEdition(type, id, title, listFormat, listCount, publicationYear);
+                break;
+            case BOOK:
+                Genre genre = Genre.valueOf(s.substring(indexEnd + 1).toUpperCase());
+                edition = new Book(type, id, title, listFormat, listCount, publicationYear, genre);
+                break;
+            case NEWSPAPER:
+                NewspaperType newspaperType = NewspaperType.valueOf(s.substring(indexEnd + 1).toUpperCase());
+                edition = new Newspaper(type, id, title, listFormat, listCount, publicationYear, newspaperType);
+                break;
+            case ENCYCLOPEDIA:
+                EncyclopediaType encyclopediaType = EncyclopediaType.valueOf(s.substring(indexEnd + 1).toUpperCase());
+                edition = new Encyclopedia(type, id, title, listFormat, listCount, publicationYear, encyclopediaType);
+                break;
+            default:
+                edition = null;
+        }
+
+        return edition;
+    }
+
+}
